36bf850fc47b549d7e6b172a153d3c487820f526
JasCastro21 <jasminecastr0@edu.unifor.br>
2024-02-28 19:38:29 -0300

    corrigindo erro

--- rpg/routes/tab.routes.tsx
+++ rpg/routes/tab.routes.tsx
@@ -1,51 +1,30 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
 
-import TabRoutes from './tab.routes';
-import Perfil from '../screens/Perfil';
-import Inicial from '../screens/Inicial';
 
-const Drawer = createDrawerNavigator();
+import Feed from '../screens/Feed';
+import Notificacao from '../screens/Notificacao';
 
-export default function DrawerRoutes() {
-    return (
-        <Drawer.Navigator 
-            screenOptions={{
-                title: ' ',
-                drawerStyle: {
-                    backgroundColor: '#8B0000', // Defina a cor de fundo desejada aqui
-                },
-                // Outras opções globais do screenOptions
-            }}
-        >
-            <Drawer.Screen 
-                name='Home'  
-                component={TabRoutes} 
-                options={{
-                    drawerIcon: ({ color, size }) => <Feather name='home' color={color} size={size} />,
-                    drawerLabel: 'Início' 
-                }}
-            />
+const Tab = createBottomTabNavigator();
 
-            <Drawer.Screen 
-                name='Perfil'  
-                component={Perfil} 
-                options={{
-                    drawerIcon: ({ color, size }) => <Feather name='user' color={color} size={size} />,
-                    drawerLabel: 'Meu Perfil',
-                }}
-            />
-
-            <Drawer.Screen 
-                name='Sair'  
-                component={Inicial} 
-                options={{
-                    drawerIcon: ({ color, size }) => <Feather name='log-out' color={color} size={size} />,
-                    drawerLabel: 'Sair',
-                    headerShown: false 
-                }}
-            />
-        </Drawer.Navigator>
-    );
+export default function TabRoutes() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false}}>
+      <Tab.Screen
+        name='Feed'
+        component={Feed}
+        options={{
+            tabBarIcon: ({color, size}) => <Feather name='home' color={color} size={size}/>
+        }}
+      />
+      <Tab.Screen
+        name='Notificacao'
+        component={Notificacao}
+        options={{
+            tabBarIcon: ({color, size}) => <Feather name='bell' color={color} size={size}/>
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
